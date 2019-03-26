2757995111b6818d95755d1654d9d331d4e90c21
Dustin Herboldshimer and Lindsey Baker <lindsey@>
2019-03-26 10:40:04 -0700

    Add buttons to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 $(function(){
-  // $(".clickable").click(function(){
-  //   $("#hide").toggle();
-  //   $("#show").toggle();
-  // });
-
+// walrus image toggle
   $(".clickable").click(function(){
     $("#show").fadeIn();
   });
   $(".clickoff").click(function(){
     $("#show").fadeOut();
   });
+
+  //change background colors
+  $("button#green").click(function() {
+  $("body").removeClass("yellow-background red-background");
+  $("body").addClass("green-background");
+});
+
+$("button#yellow").click(function() {
+  $("body").removeClass("green-background red-background");
+  $("body").addClass("yellow-background");
+});
+
+$("button#red").click(function() {
+  $("body").removeClass("green-background yellow-background");
+  $("body").addClass("red-background");
+});
 });
